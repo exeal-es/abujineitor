9dea4420a2a35e07f41234f93f1f3698cfeff280
Pedro Pardal <ppardalj@gmail.com>
2024-04-08 21:14:23 +0200

    refactor: remove duplication from tests

--- Exeal.Abujineitor.Tests/InjectorShould.cs
+++ Exeal.Abujineitor.Tests/InjectorShould.cs
@@ -2,48 +2,42 @@ namespace Exeal.Abujineitor;
 
 public class InjectorShould
 {
+    private readonly Injector injector;
+
+    public InjectorShould()
+    {
+        injector = new Injector();
+    }
+
     [Fact]
     public void ReturnRegisteredInstance()
     {
-        // Arrange
-        Injector injector = new Injector();
         var registeredInstance = new TestService();
         injector.Register(registeredInstance);
 
-        // Act
         var returnedInstance = injector.GetService<TestService>();
 
-        // Assert
         Assert.Same(returnedInstance, registeredInstance);
     }
     
     [Fact]
     public void FailWhenAttemptingToGetNotRegisteredType()
     {
-        // Arrange
-        Injector injector = new Injector();
-
-        // Act
         Action action = () => injector.GetService<TestService>();
 
-        // Assert
         Assert.Throws<InjectorException>(action);
     }
     
     [Fact]
     public void SupportMultipleTypes()
     {
-        // Arrange
-        Injector injector = new Injector();
         var testInstance = new TestService();
         var anotherTestInstance = new AnotherTestService();
         injector.Register(testInstance);
         injector.Register(anotherTestInstance);
 
-        // Act
         var returnedInstance = injector.GetService<TestService>();
 
-        // Assert
         Assert.Same(returnedInstance, testInstance);
     }
 }
